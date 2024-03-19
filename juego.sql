e73d0725cab566a6319b8981629b14940526ae1d
JessicaOlivares <jessicaolivares863@gmail.com>
2024-03-19 15:56:14 -0300

    avance en el juego hasta que estaba recolectando cosas sin dueño

---
+++ juego.sql
@@ -0,0 +1,7 @@
+    SELECT * FROM inhabitant
+    SELECT * FROM inhabitant WHERE state = 'friendly'
+    SELECT * FROM inhabitant WHERE job = 'weaponsmith' and state= 'friendly'
+    SELECT * FROM inhabitant WHERE job like '%smith' and state= 'friendly'
+    SELECT personid From inhabitant where name = 'Stranger'
+    SELECT gold From inhabitant where name = 'Stranger'
+    SELECT * FROM item where owner is null
