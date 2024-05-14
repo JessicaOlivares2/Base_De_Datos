e4ace3c0b279fb4784b35dd09b4d1f7c2dc8de8f
Jessica Olivares <jessicaolivares863@gmail.com>
2024-05-14 15:47:14 -0300

    aregle el ejercicio 2

--- Evaluación/ejercicio2.sql
+++ Evaluación/ejercicio2.sql
@@ -2,6 +2,7 @@
 -- (marca, modelo y patente) de todos los
 -- varones más de 70 años, elegir el orden
 -- en que se muestran 
-SELECT plate_number,car_model,car_make,age,gender from drivers_license
+SELECT p.name, d.plate_number,d.car_make,d.car_model,d.gender,d.age FROM person p
+JOIN drivers_license d ON p.license_id = d.id
 where age > 70 and gender = 'male'
 ORDER by age ASC
