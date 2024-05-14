496470e613668cfe27cb009ecf67f7767929db70
Jessica Olivares <jessicaolivares863@gmail.com>
2024-05-14 16:28:23 -0300

    arregle el 5

--- Evaluación/ejercicio5.sql
+++ Evaluación/ejercicio5.sql
@@ -1,6 +1,6 @@
 -- 5) Encontrar la edad mínima, máxima y promedio
 -- de las mujeres que tienen licencia de conducir
-SELECT p.name, d.plate_number,d.car_make,d.car_model,min(d.age) as minima_edad,
+SELECT min(d.age) as minima_edad,
 max(d.age)  as edad_maxima , avg(d.age) as edad_promedio ,d.gender FROM person p
 JOIN drivers_license d ON p.license_id = d.id
 where gender = 'female'
