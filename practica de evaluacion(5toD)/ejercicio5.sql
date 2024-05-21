bed5fe391f79f9be2dad632c83256509592e8949
Jessica Olivares <jessicaolivares863@gmail.com>
2024-05-21 15:48:44 -0300

    arregle un join que tenia de mas

--- practica de evaluacion(5toD)/ejercicio5.sql
+++ practica de evaluacion(5toD)/ejercicio5.sql
@@ -1,6 +1,5 @@
 -- 5) Encontrar la edad mínima, máxima y promedio
 -- de los varones que tienen licencia de conducir
 SELECT min(d.age) as minima_edad,
-max(d.age)  as edad_maxima , avg(d.age) as edad_promedio ,d.gender FROM person p
-JOIN drivers_license d ON p.license_id = d.id
+max(d.age)  as edad_maxima , avg(d.age) as edad_promedio ,d.gender FROM drivers_license d
 where gender = 'male'
