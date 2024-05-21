bb127bb8319b68700afbdaa83484bc3e6f382c78
Jessica Olivares <jessicaolivares863@gmail.com>
2024-05-21 17:10:40 -0300

    arregle algunas cosas

--- practica de evaluacion(5toD)/ejercicio6.sql
+++ practica de evaluacion(5toD)/ejercicio6.sql
@@ -2,7 +2,7 @@
 -- marca, ordenadas desde la que tiene más 
 -- autos a la que tiene menos.  Descartar
 -- las marcas que tienen menos de 100 autos
-SELECT  car_make,count(*) as autosMarca,car_model,plate_number from drivers_license d
+SELECT  car_make,count(*) as autosMarca from drivers_license d
 GROUP by car_make
 HAVING autosMarca >100
 ORDER by autosMarca DESC
